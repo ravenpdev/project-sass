1993e24276523b82aea63d21624015ea7de996ff
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2025-01-16 14:56:09 +0800

    chore: configure AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -21,6 +27,50 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureDates();
+        $this->configureUrls();
+        $this->configureVite();
+    }
+
+    /**
+     * Do not allow database desctructive commands in production
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(App()->isProduction());
+    }
+
+    /**
+     * Prevent lazy loading, silently discarding attributes, accessing missing attributes
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(! App()->isProduction());
+    }
+
+    /**
+     * Use immutable dates
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Force to use https
+     */
+    private function configureUrls(): void
+    {
+        URL::forceScheme('https');
+    }
+
+    /**
+     * Configure vite
+     */
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
     }
 }
